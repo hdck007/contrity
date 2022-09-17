99d3d3d6f3c3fb1aef3c3e2211e35333f6a338da
hari <haridarshanofficial@gmail.com>
2022-09-17 22:59:56 +0530

    chore: removed unused files

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,17 @@
 import { WagmiConfig } from 'wagmi';
 import '../styles/global.css';
 import PropTypes from 'prop-types';
+// import { useEffect } from 'react';
 import client from '../lib/wagmi/chain-config';
+// import getWalletInstance from '../lib/arcana/authprovider';
 
 function MyApp({ Component, pageProps }) {
+
+	// useEffect(async () => {
+	// 	const provider = await getWalletInstance();
+	// 	console.log(provider)
+	// }, [])
+
 	return (
 		<WagmiConfig client={client}>
 			<Component {...pageProps} />

5ee9e58d3e222b94455a394ee5f3f6dd8fbd3783
hari <haridarshanofficial@gmail.com>
2022-10-01 22:49:47 +0530

    refactor: a simple back button for all the pages

---
+++ client/src/components/backbutton.jsx
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router';
+
+export default function BackButton() {
+	const router = useRouter();
+
+	return (
+		<button
+			type='button'
+			onClick={() => router.back()}
+			className='btn btn-circle flex gap-2 rounded-full items-center hover:bg-purple-400 hover:text-white'
+		>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				fill='none'
+				viewBox='0 0 24 24'
+				strokeWidth='1.5'
+				stroke='currentColor'
+				className='w-6 h-6'
+			>
+				<path
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
+				/>
+			</svg>{' '}
+			Back
+		</button>
+	);
+}
